15e9d11163855c3ed97e6f77661ea5c77171a537
Your Name <you@example.com>
2021-01-25 16:47:59 -0400

    Tabs

---
+++ src/components/CustomTabBar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+
+function CustomTabBar( {state, descriptors, navigation} ) {
+
+    return (
+        <View style={styles.container}>
+            {state.routes.map((route, index) => {
+                const options = descriptors[route.key].options;
+
+                let label = route.name;
+                if(options.tabBarLabel != undefined) {
+                    label = options.tabBarLabel;
+                } else if(options.title != undefined) {
+                    label = options.title;
+                }
+
+                const isFocused = state.index === index;
+
+                const handleTabPress = () => {
+                    navigation.navigate( route.name );
+                }
+
+                if(route.name == 'TabHome') {
+                    return (
+                        <TouchableHighlight key={index} style={styles.middleTab}
+                            onPress={handleTabPress}>
+                            <Text style={[styles.label, isFocused?styles.labelFocused:null]}>{label}</Text>
+                        </TouchableHighlight>
+                    );
+                } else {
+                    return (
+                        <TouchableHighlight key={index} style={styles.tab}
+                            onPress={handleTabPress}
+                            underlayColor="transparent"
+                        >
+                            <Text style={[styles.label, isFocused?styles.labelFocused:null]}>{label}</Text>
+                        </TouchableHighlight>
+
+                    );
+                }
+            })}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'row',
+        backgroundColor: '#CCC'
+    },
+    tab:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+    },
+    middleTab:{
+        //flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 70,
+        height: 70,
+        backgroundColor:"#00FF00",
+        marginTop: -20,
+        borderRadius: 35
+    },
+    label:{
+        fontSize: 12,
+        color: '#FF0000'
+    },
+    labelFocused:{
+        fontSize: 12,
+        color: '#0000FF'
+    }
+});
+
+export default CustomTabBar;
--- src/navigators/MainTab.js
+++ src/navigators/MainTab.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import CustomTabBar from '../components/CustomTabBar';
+
+import TabConfigScreen from '../pages/TabConfigScreen';
 import TabHomeScreen from '../pages/TabHomeScreen';
 import TabAboutScreen from '../pages/TabAboutScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default () => (
-    <Tab.Navigator>
-        <Tab.Screen name="TabHome" component={TabHomeScreen} />
-        <Tab.Screen name="TabAbout" component={TabAboutScreen} />
+    <Tab.Navigator
+        initialRouteName="TabHome"
+        tabBar={(props) => <CustomTabBar {...props} />}
+    >
+        <Tab.Screen name="TabAbout" component={TabAboutScreen} options={{tabBarLabel:'Sobre'}} />
+        <Tab.Screen name="TabHome" component={TabHomeScreen} options={{tabBarLabel:'Home'}} />
+        <Tab.Screen name="TabConfig" component={TabConfigScreen} options={{tabBarLabel:'Config'}} />
     </Tab.Navigator>
         
 );
